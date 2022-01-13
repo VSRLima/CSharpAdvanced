008effacd0ea13473ac8f9b66925684ced016e9f
Vinicius Reis <vinicius.reis@soterotech.com.br>
2022-01-12 21:52:50 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
