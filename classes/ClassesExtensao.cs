899d513a088e93075054bb8b67cfa6cce106e575
Vinicius Reis <vinicius.reis@soterotech.com.br>
2022-02-10 20:25:25 -0300

    forgot to add new ClassExtensao file

---
+++ classes/ClassesExtensao.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CsharpAdvanced
+{
+    public static class DataSetExtension 
+    {
+        public static void WriteJSON(this DataSet ds, string filename)
+        {
+            //...
+        }
+    }
+}
