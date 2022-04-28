5c59ff206efcbd82d961bcab37d73d94f7a572c6
Vinicius Reis <vinicius.reis@soterotech.com.br>
2022-04-27 22:08:07 -0300

    Stack

--- Program.cs
+++ Program.cs
@@ -12,63 +12,30 @@ namespace ValueRef
 {
     class Program
     {
-        public class Pedido
+        public class Documento
         {
-            public int CodPedido;
-            public string Cliente;
-            public double Valor;
+            public int Codigo;
+            public string? Tipo;
+            public string? Nome;
         }
         static void Main(string[] args)
         {
-            var pedidos = new Dictionary<int, Pedido>();
-            var p1 = new Pedido() { CodPedido = 1, Cliente = "Vinicius", Valor = 4000 };
-            var p2 = new Pedido() { CodPedido = 2, Cliente = "Mayanne", Valor = 4500 };
-            var p3 = new Pedido() { CodPedido = 3, Cliente = "Simba", Valor = 400 };
+            var d1 = new Documento() { Codigo = 1, Nome = "Planilha", Tipo = "xls"};
+            var d2 = new Documento() { Codigo = 2, Nome = "Artigo", Tipo = "doc"};
+            var d3 = new Documento() { Codigo = 3, Nome = "Apresentação", Tipo = "ppt"};
+            var pilha = new Stack<Documento>();
+            pilha.Push(d1);
+            pilha.Push(d2);
+            pilha.Push(d3);
+            Console.WriteLine(pilha.Count);
+            var obj = pilha.Pop();
+            Console.WriteLine(obj?.Nome);
 
-            pedidos.Add(1, p1);
-            pedidos.Add(2, p2);
-            pedidos.Add(3, p3);
-            Console.WriteLine("Nomes dos clientes que fizeram pedidos: ");
-            foreach (Pedido p in pedidos.Values)
-            {
-                Console.WriteLine(p.Cliente);
-            }
+            Console.WriteLine(pilha?.Peek().Nome);
 
-            Console.WriteLine("Pedidos:");
-            foreach (KeyValuePair<int, Pedido> kvp in pedidos)
-            {
-                Console.WriteLine($"{kvp.Key} = {kvp.Value.Cliente}");
-            }
-
-            Console.WriteLine($"Soma dos pedidos: {pedidos.Sum(p => p.Value.Valor)}");
+            while(pilha.Count > 0)
+                Console.WriteLine(pilha.Pop().Nome);
         }
 
-        private static void RefTypeInt()
-        {
-            PosicaoClass pos1 = new PosicaoClass();
-            pos1.X = 10;
-            pos1.Y = 20;
-            PosicaoClass pos2 = new PosicaoClass();
-            pos2.X = 30;
-            pos2.Y = 40;
-            pos2 = pos1; // apontando ambos para a mesma posição de memória
-            pos2.X = 50;
-
-            pos1.Dobrar(pos1);
-            pos2.Dobrar(pos2);
-            Console.WriteLine(pos1.X);
-            Console.WriteLine(pos1.Y);
-            Console.WriteLine(pos2.X);
-            Console.WriteLine(pos2.Y);
-        }
-
-        private static void ValueTypeInt()
-        {
-            int x = 10;
-            int y = 20;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.ReadLine();
-        }
     }
 }
